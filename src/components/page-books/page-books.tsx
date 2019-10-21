db5c5370bb790c7efbbddc6d9b4b4ce0a89baee0
Cody Burleson <cody.burleson@gmail.com>
2019-10-21 11:12:36 -0600

    Update Books page with new thumb and list type.

--- src/components/page-books/page-books.tsx
+++ src/components/page-books/page-books.tsx
@@ -8,6 +8,18 @@ export class PageBooks {
 
   title = 'Books';
 
+  writingItems = [
+    {
+      id: "/cage",
+      title: "The Cage",
+      teaser: "The epic true story of the Special Landing Force of 1st Batallion, 3rd Marines in the Vietnam War.",
+      thumbnail: "https://s3.us-east-2.amazonaws.com/codyburleson.com/images/books/cage-book-thumb.jpg",
+      datePublished: "",
+      dateModified: "Oct 21, 2019"
+    }
+  ]
+
+
   componentWillLoad() {
       document.title = this.title;
   }
@@ -24,24 +36,25 @@ export class PageBooks {
       </ion-header>,
 
       <ion-content class="ion-padding">
-        <h1>What I'm Writing</h1>
+        <h2>What I'm Writing</h2>
+        <ion-list>
+        {this.writingItems.map((item) =>
+            <ion-item href={item.id}>
+              <ion-thumbnail slot="start">
+                <img src={item.thumbnail}/>
+              </ion-thumbnail>
+              <ion-label text-wrap>
+              {item.title}
+              <p innerHTML={item.teaser}></p>
+              </ion-label>
+            </ion-item>
+        )}
+        </ion-list>
         
-        <ion-card>
-          <img src="https://gallery.mailchimp.com/085bae426fecfc73c590d0ba3/images/c404108a-8afb-4905-ad08-5b763905dce8.jpg" />
-          <ion-card-header>
-            <ion-card-subtitle>1967-1968, Vietnam</ion-card-subtitle>
-            <ion-card-title>The Cage</ion-card-title>
-          </ion-card-header>
-          <ion-card-content>
-            <p>This is the epic true story of the Special Landing Force of 1st Batallion, 3rd Marines in the Vietnam War.</p>
-            <ion-button color="primary" href="/books/cage">Read more...</ion-button>
-          </ion-card-content>
-        </ion-card>
-
         <h1>What I'm Reading</h1>
         <h1>What I Want to Read</h1>
         <h1>What I've Read</h1>
-
+        
       </ion-content>
     ];
   }
