340732c822e3c34a122917a96ed36bbbfb041366
Cody Burleson <cody.burleson@gmail.com>
2020-04-04 10:38:30 -0600

    Version 1.6.3

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,5 +1,5 @@
 // SITEVERSION must always be line #2 because it is replaced by line number
-export const SITEVERSION = "1.6.2";
+export const SITEVERSION = "1.6.3";
 export const SITENAME = "Cody Burleson";
 export const DISQUS_SHORTNAME = "codyburleson-com";
 
